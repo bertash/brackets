3f753bc049b250afdbfc14474788b5af6e084088
Bertash <y.bertash@gmail.com>
2020-10-14 00:25:29 +0300

    feat:add solution for the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  for (let i = 0; i < bracketsConfig.length;) {
+    let cuter = bracketsConfig[i].join('');
+
+    if (str.includes(cuter)) {
+      str = str.replace(cuter, '');
+      i = 0;
+    } else {
+      i++;
+    }
+  }
+  if (str.length == 0) {
+  return true;
+  } else {
+  return false;
+  }
+  
+};
